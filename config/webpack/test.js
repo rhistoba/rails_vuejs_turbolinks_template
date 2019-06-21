24b90fda56f7a36dae2254ba4bca5bf2886c3402
rhistoba <fujitakeisound@gmail.com>
2019-06-21 22:51:17 +0900

    fix webpack config

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const config = Object.assign(environment.toWebpackConfig(), require('./vue.config'))
 
-module.exports = environment.toWebpackConfig()
+module.exports = config
